fae11f72c01cd8cfd96cb3669a26fe702b2d1c3f
Fabien Pardo <fabien009@hotmail.com>
2015-02-13 14:55:19 +0100

    ftp modif

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -12,14 +12,14 @@ class Cocktail < ActiveRecord::Base
   has_attached_file :picture,
     styles: { medium: "300x300>", thumb: "100x100>" },
     storage: :ftp,
-    passive: true,
     path: "www/:attachment/:id/:style/:filename",
     url: "http://www.lowclass.net/:attachment/:id/:style/:filename",
     ftp_servers: [
       {
         host:      ENV["FTP_HOST"],
         user:      ENV["FTP_USER"],
-        password:  ENV["FTP_PASSWORD"]
+        password:  ENV["FTP_PASSWORD"],
+        passive: true
       },
     ],
     ftp_connect_timeout: 5,
