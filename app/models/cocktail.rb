3a211dc79b3203e8f1a10bbd40ca8275c0fb9ac8
Fabien Pardo <fabien009@hotmail.com>
2015-02-13 14:29:25 +0100

    ftp modif

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -11,17 +11,18 @@ class Cocktail < ActiveRecord::Base
 
   has_attached_file :picture,
     styles: { medium: "300x300>", thumb: "100x100>" },
-    :storage => :ftp,
-    :path => "www/:attachment/:id/:style/:filename",
-    :url => "https://www.lowclass.net/:attachment/:id/:style/:filename",
-    :ftp_servers => [
+    storage: :ftp,
+    path: "www/:attachment/:id/:style/:filename",
+    url: "https://www.lowclass.net/:attachment/:id/:style/:filename",
+    ftp_servers: [
       {
-        :host     => ENV["FTP_HOST"],
-        :user     => ENV["FTP_USER"],
-        :password => ENV["FTP_PASSWORD"]
+        host:      ENV["FTP_HOST"],
+        user:      ENV["FTP_USER"],
+        password:  ENV["FTP_PASSWORD"]
       },
     ],
-    :ftp_ignore_failing_connections => true
+    ftp_ignore_failing_connections: true
+    passive: true
 
   validates_attachment_content_type :picture,
     content_type: /\Aimage\/.*\z/
